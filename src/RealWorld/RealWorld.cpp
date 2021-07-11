f9849308987263629ad83db3b49ac3ea4ceadc35
ttaggg-bin <bin1991@live.com>
2021-07-11 15:49:21 +0800

    add translator

--- src/RealWorld/RealWorld.cpp
+++ src/RealWorld/RealWorld.cpp
@@ -1,12 +1,40 @@
 #include <QApplication>
 #include <QMainWindow>
-
+#include <qtranslator.h>
 #include "RealWorld.h"
 #include "mainwindow.h"
+
+void loadTranstor(const QApplication& app)
+{
+    QString lang_dir = app.applicationDirPath() + "/lang/";
+    
+    QLocale ql;
+    const char* lang_file = NULL;
+    QLocale::Language lang = ql.language();
+    switch (lang)
+    {
+    case QLocale::Chinese:
+        lang_file = ":/lang/zh-cn.qm";
+        break;
+    default:
+        lang_file = NULL;
+    }
+
+    if (lang_file)
+    {
+        QTranslator* translator = new QTranslator;
+        translator->load(":/lang/zh-cn.qm");
+        app.installTranslator(translator);
+    }
+    return;
+}
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
+
+    loadTranstor(a);
+
     MainWindow win;
     win.show();
     return a.exec();
---
+++ src/RealWorld/lang/zh-cn.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="31"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="38"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="47"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="52"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="13"/>
+        <source>Output window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="53"/>
+        <source>Load file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="62"/>
+        <source>Open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="68"/>
+        <source>Select file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
